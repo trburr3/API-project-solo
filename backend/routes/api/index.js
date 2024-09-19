9da761bd2ab16c43c36aa7b1e5cc86b6d570193d
Xiaoxue <xiaoxuew895@gmail.com>
2024-09-19 15:31:24 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
-
-
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 // router.get(
 //   '/restore-user',
@@ -36,8 +40,8 @@ router.use(restoreUser);
 //   return res.json({ user: user });
 // });
 
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
 
 module.exports = router;
