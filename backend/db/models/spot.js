e1ccf386eb8d605b532ca310a5660b013399f7a9
Xiaoxue <xiaoxuew895@gmail.com>
2024-09-23 15:41:06 -0700

    d1-db

---
+++ backend/db/models/spot.js
@@ -0,0 +1,108 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      Spot.belongsTo(models.User, {
+        foreignKey: "ownerId"
+      });
+      Spot.hasMany(models.Review, {
+        foreignKey: "spotId",
+        onDelete: "CASCADE"
+      });
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: "spotId",
+        onDelete: "CASCADE"
+      });
+      Spot.hasMany(models.Booking, {
+        foreignKey: "spotId",
+        onDelete: "CASCADE"
+      });
+    }
+  }
+Spot.init({
+  ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+    unique: true,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+  },
+  lat: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    validate: {
+      min: -90,
+      max: 90,
+    },
+  },
+  lng: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    validate: {
+      min: -180,
+      max: 180,
+    },
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    validate: {
+      min: 0
+    },
+  },
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
